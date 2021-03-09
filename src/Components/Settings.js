b64d4ec7f38c15214342a4882f3c0ce92718aec5
Alec Sloan <sloan.alec@gmail.com>
2021-03-08 21:40:43 -0800

    Switch Data Fetch Notification Timeout setting to be my custom IntervalSelector instead of a TextField

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -201,19 +201,18 @@ class Settings extends Component {
                       variant="outlined"
                   />
               </div>
-              <div className="row">
-                  <TextField
-                      InputLabelProps={{
-                          shrink: true,
-                      }}
-                      label="Fetch Notification Timeout"
-                      onInputCapture={event => this.props.editSetting('autoHideFetchNotification', event.target.value)}
-                      size="small"
-                      type="number"
-                      value={this.props.settings.autoHideFetchNotification}
-                      variant="outlined"
-                  />
-              </div>
+              <IntervalSelector
+                  value={this.props.settings.autoHideFetchNotification}
+                  label="Data Fetch Notification Timeout"
+                  labelPlacement="top"
+                  max="Minute"
+                  min="Second"
+                  onChange={value => this.props.editSetting('autoHideFetchNotification', value)}
+                  selectFieldClasses="col-8"
+                  size="small"
+                  textFieldClasses="col-4"
+                  variant="outlined"
+              />
               <div className="row">
                   <div className="col-sm-4">
                       <FormControlLabel
