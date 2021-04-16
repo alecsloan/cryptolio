e9cacce038d471e8f1d1c38eaab412dc73776408
Alec Sloan <sloan.alec@gmail.com>
2021-04-15 23:25:42 -0700

    Show sorting on the main screen for the mobile table view and hide it from the settings panel

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -198,6 +198,7 @@ class Settings extends Component {
                     <MenuItem key='percent_change_7d' value='percent_change_7d'>7 Day Change</MenuItem>
                   </Select>
                 }
+                hidden={(this.props.settings.renderStyle === 'table' && window.innerWidth <= 500)}
                 label='Sort By'
                 labelPlacement='top'
                 value='top'
