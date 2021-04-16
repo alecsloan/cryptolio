c30b81ce172e8122320e3b75f6127afb8645b4a6
Alec Sloan <sloan.alec@gmail.com>
2021-04-15 23:27:10 -0700

    Hide Portfolio Change Period setting from panel mobile table viewer

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -297,6 +297,7 @@ class Settings extends Component {
                     <MenuItem key='percent_change_7d' value='percent_change_7d'>7 Day Change</MenuItem>
                   </Select>
                 }
+                hidden={(this.props.settings.renderStyle === 'table' && window.innerWidth <= 500)}
                 label='Portfolio Change Period'
                 labelPlacement='top'
                 value='top'
